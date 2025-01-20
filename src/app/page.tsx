e283266585ee9f06d6faf13aad27c3e620fdb7f1
Sahil-Shadwal <sahilshadwal@gmail.com>
2025-01-21 00:39:50 +0530

    project section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/Hero";
-import { Header } from "@/sections/Header";
+// import { Header } from "@/sections/Header";
+import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <div className="bg-slate-950">
+      {/* <Header /> */}
       <Hero />
+      {/* <TapeSection /> */}
+      <ProjectsSection />
+      <TapeSection />
     </div>
   );
 }
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
-import StarIcon from "@/assets/icons/star.svg"
+// import StarIcon from "@/assets/icons/star.svg"
+import { StarIcon } from "lucide-react";
 import { Fragment } from "react";
 
 const words = [
@@ -12,7 +13,7 @@ const words = [
   "Usable",
   "Reliable",
   "Lightning-fast",
-]
+];
 
 export const TapeSection = () => {
   return (
@@ -22,12 +23,14 @@ export const TapeSection = () => {
           <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
             {[...new Array(2)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
-                  {words.map(word => (
-                    <div key={word} className="inline-flex gap-4 items-center">
-                      <span className="text-gray-900 uppercase font-extrabold text-sm">{word}</span>
-                      <StarIcon className="size-6 text-gray-900 -rotate-12" />
-                    </div>
-                  ))}
+                {words.map((word) => (
+                  <div key={word} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
               </Fragment>
             ))}
           </div>
